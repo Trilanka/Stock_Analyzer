2f860d1956cb9db5a9a8a9c4923f396f80e91293
Trilanka <tstrilanka@gmail.com>
2025-05-06 13:01:06 +0530

    api.py

--- api.py
+++ api.py
@@ -9,16 +9,16 @@ DATA_PATH = '/home/ubuntu/Scrapper/Stock_Analyzer/structured_data/structured_com
 @app.route('/api/data', methods=['GET'])
 
 def get_data():
-    # try:
-    #     if os.path.exists(DATA_PATH):
-    #         with open(DATA_PATH, 'r') as file:
-    #             data = json.load(file)
-    #         return jsonify(data)
-    #     else:
-    #         return jsonify({"error": "Data file not found"}), 404
-    # except Exception as e:
-    #     return jsonify({"error": str(e)}), 500
-    return "Hello, World!"
+    try:
+        if os.path.exists(DATA_PATH):
+            with open(DATA_PATH, 'r') as file:
+                data = json.load(file)
+            return jsonify(data)
+        else:
+            return jsonify({"error": "Data file not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=800, debug=True)
